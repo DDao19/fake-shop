ff4730cdc1918cd00ed8878511dd11b5e02c73c9
ddao19 <dung.dao19@outlook.com>
2021-06-14 20:47:47 -0500

    React Redux practice project

---
+++ src/components/ProductListing.js
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from "../redux/actions/productActions";
+import axios from "axios";
+import Product from "./Product";
+
+const ProductListing = () => {
+  const products = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((error) => {
+        console.log("Error", error);
+      });
+    dispatch(setProducts(response.data));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="ui grid container">
+      <Product />
+    </div>
+  );
+};
+
+export default ProductListing;
